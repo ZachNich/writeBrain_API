659c6917bd5d5d27c62ffcf6220f096591582125
Zach Nicholson <raiste.doctow@gmail.com>
2020-09-17 10:46:45 -0500

    sprint models updated, fixtures finished

--- writeBrain/models/sprint.py
+++ writeBrain/models/sprint.py
@@ -8,8 +8,8 @@ class Sprint(models.Model):
     body = models.CharField(max_length=500000)
     started_at = models.DateTimeField(auto_now=False)
     ended_at = models.DateTimeField(auto_now=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    story = models.OneToOneField(Story, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
     mood_before = models.ForeignKey(Mood, related_name="mood_before", on_delete=models.DO_NOTHING)
     mood_after = models.ForeignKey(Mood, related_name="mood_after", on_delete=models.DO_NOTHING)
 
